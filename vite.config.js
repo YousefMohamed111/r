0d0d0e18f2f55214698e7f9dd314233fce8b4f10
Your Name <your.email@example.com>
2024-06-25 22:07:08 +0300

    react

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,8 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   base: "/r/",
   plugins: [react()],
+  build: {
+    outDir: 'build',  // تغيير المجلد الافتراضي للإخراج إلى 'build'
+  },
 })
+
